5e7fe6cd2056561505ac9d708ba59d3f94af3bb9
Gagik Simonyan <simonyan.gagik@softconstruct.com>
2021-10-14 15:11:51 +0400

    created 'dashboard' UI

---
+++ src/app/modules/dashboard/dashboard-routing.module.ts
@@ -0,0 +1,22 @@
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: DashboardComponent,
+    children: [
+      {
+        path: ':userId',
+        
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class DashboardRoutingModule { }
